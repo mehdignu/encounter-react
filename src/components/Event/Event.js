df7132b9ede7b25bfc225d1ee8c50c1b489d61a3
mehdi dridi <mehdignu@users.noreply.github.com>
2019-03-09 12:24:21 +0100

    styling the messages in the chat box

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -5,33 +5,51 @@ import Paper from "@material-ui/core/Paper";
 
 const event = (props) => (
 
-    <Paper className={cls.content} elevation={1}>
+        <Paper className={cls.content} elevation={1}>
 
-        <div className={cls.wrapper}>
-
-
-
-            <ul>
-
-                <div className={cls.cont}>
-                    <img src="https://www.w3schools.com/w3images/bandmember.jpg" alt="Avatar" className={cls.right}/>
-                    <li className={cls.me}>By this User, secondmessage</li>
-                    <span className={cls["time-right"]}>11:02</span>
-                </div>
-
-                <div className={cls.cont}>
-                    <img src="https://www.w3schools.com/w3images/avatar_g2.jpg" alt="Avatar"/>
-                    <li className={cls.him}>By this User, first message</li>
-                    <span className={cls["time-left"]}>11:05</span>
-                </div>
-
-            </ul>
+            <div className={cls.wrapper}>
 
 
-            <input placeholder="Type a message" className={cls.sendTab}/>
-        </div>
-    </Paper>
+                <ul>
 
-);
+                    <div className={cls.cont}>
+                            <img src="https://www.w3schools.com/w3images/bandmember.jpg" alt="Avatar"
+                                 className={cls.right}/>
+
+
+                        <div className={cls.userInfo}>
+
+                        <span className={cls.name}>user1</span>
+                            <span className={cls["time-right"]}>11:02</span>
+                        </div>
+
+                        <li className={cls.msg}>By this User, secondmessage Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid amet aperiam ipsa laboriosam laudantium quae quas sequi totam, vitae! Ad at cum dolorum, ducimus eos provident quia quod temporibus.</li>
+                    </div>
+
+                    <div className={cls.cont}>
+                            <img src="https://www.w3schools.com/w3images/bandmember.jpg" alt="Avatar"
+                                 className={cls.right}/>
+
+
+                        <div className={cls.userInfo}>
+
+                        <span className={cls.name}>user1</span>
+                            <span className={cls["time-right"]}>11:02</span>
+                        </div>
+
+                        <li className={cls.msg}>By this User, secondmessage</li>
+                    </div>
+
+
+                </ul>
+
+
+                <input placeholder="Type a message" className={cls.sendTab}/>
+            </div>
+
+        </Paper>
+
+    )
+;
 
 export default event;
