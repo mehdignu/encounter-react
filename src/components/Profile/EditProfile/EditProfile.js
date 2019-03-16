63f71651a7d15913750a33a86d7fe8e4b32ed2a1
mehdi dridi <mehdignu@users.noreply.github.com>
2019-03-16 11:10:28 +0100

    add the server

--- src/components/Profile/EditProfile/EditProfile.js
+++ src/components/Profile/EditProfile/EditProfile.js
@@ -50,6 +50,26 @@ class EditProfile extends React.Component {
         this.setState({[name]: event.target.value});
     };
 
+    componentDidMount() {
+        // Call our fetch function below once the component mounts
+        this.callBackendAPI()
+            // .then(res => this.setState({data: res.express}))
+            .then(res =>  console.log(res))
+            .catch(err => console.log(err));
+
+    }
+
+
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+    callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body;
+    };
 
     render() {
         const {classes} = this.props;
