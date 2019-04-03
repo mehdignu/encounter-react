d63e16863177c7456480c5f2ac6089e0b25ba993
mehdi dridi <mehdignu@users.noreply.github.com>
2019-04-03 11:48:50 +0200

    add global params

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import Layout from './components/Layout/Layout';
 import Encounter from './containers/Encounter/Encounter';
-import {Route, Switch} from "react-router-dom";
-import About from "./components/Layout/Footer/About/About";
 import Aux from "./hoc/Aux";
 
 class App extends Component {
@@ -11,12 +9,9 @@ class App extends Component {
     render() {
         return (
             <Aux>
-
-
                 <Layout>
                     <Encounter/>
                 </Layout>
-
             </Aux>
         );
     }
