416c3fd717323cb0ccea62c626a88e6684d5cc9c
mehdi dridi <mehdignu@users.noreply.github.com>
2019-04-03 12:10:27 +0200

    .gitignore fix

---
+++ server/server_params.js
@@ -0,0 +1,17 @@
+module.exports = Object.freeze({
+
+    //chatkit
+    instanceLocator: 'v1:us1:0bbd0f2e-db34-4853-b276-095eb3ef4762',
+    key: '898c19ad-e17b-4e2a-9dfb-ecd215327d50:aJRKgR09pI+cPc+hGsT58d0fTEXxmVnoVk50Fs52Y4g=',
+
+    //pusher
+    appId: '741209',
+    keyPusher: 'b3c4b499cc2b3ff03699',
+    secret: 'eda0320d9a352b5470b7',
+
+    //cloudinary
+    cloud_name: 'drtbzzsis',
+    api_key: '839589289496339',
+    api_secret: 'vn8Yl0z3k9_o74lm3avOEWBBc4s',
+
+});
