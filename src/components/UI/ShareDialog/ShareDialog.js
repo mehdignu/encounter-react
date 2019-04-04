37cee518b732998cbb660df5c710a90cc1e6811c
mehdi dridi <mehdignu@users.noreply.github.com>
2019-04-05 00:49:07 +0200

    change show to share link

--- src/components/UI/ShareDialog/ShareDialog.js
+++ src/components/UI/ShareDialog/ShareDialog.js
@@ -64,7 +64,7 @@ class ShareDialog extends React.Component {
                                 id="name"
                                 label="Event Link"
                                 type="email"
-                                value={"https://encounter.social/show/" + this.props.dialog.eventID}
+                                value={"https://encounter.social/share/" + this.props.dialog.eventID}
                                 fullWidth
                             />
                         </DialogContent>
